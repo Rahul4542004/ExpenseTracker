65da18d2100d8fc0168a587caaf7e107b4400f9f
Rahul <madhanirahul92@gmail.com>
2024-02-27 13:36:31 +0530

    b1 commited

--- src/Components/Balance.js
+++ src/Components/Balance.js
@@ -4,11 +4,16 @@ export const Balance = ({transactions}) => {
   let amount = transactions.map(transaction => transaction.amount);
   let totalBalance = amount.reduce((acc,item) => acc += item,0);
   return (
-    <>
-        <h3>Your Balance</h3>
-        <h2 style={{
-            marginTop : '-20px',
-        }}>{totalBalance<0 && '-'}₹{totalBalance<0?-totalBalance:totalBalance}</h2>
-    </>
+    <div style={{ display: 'flex', alignItems: 'center',marginBottom : '10px',marginTop : '-20px'}}>
+    <h2 style = {{fontSize:'40px'}}>Your Balance : </h2>
+    <h2 style={{
+      color: totalBalance < 0 ? 'red' : 'green',
+      marginLeft: '5px',
+      fontSize : '40px'
+    }}>
+      {totalBalance < 0 && '-'}₹{totalBalance < 0 ? -totalBalance : totalBalance}
+    </h2>
+    {totalBalance<0 && <div><span className="warn warning"></span></div>}
+  </div>
   )
 }
